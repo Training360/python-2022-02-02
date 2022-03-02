83c3c64ae019ab1a8a974f61d31c01cc6a2810f1
vicziani <viczian.istvan@gmail.com>
2022-03-02 15:32:58 +0100

    input

---
+++ user_interface.py
@@ -0,0 +1,10 @@
+# name = input("Add meg a neved!")
+# print(name)
+
+number = input("Adj meg egy szamot, es en megszorzom ottel!")
+print(type(number))
+print(int(number) * 5)
+
+# print(int("abc")) # ValueError: invalid literal for int() with base 10: 'abc'
+
+print("End")
